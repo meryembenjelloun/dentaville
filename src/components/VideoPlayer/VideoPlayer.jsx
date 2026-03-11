31b7a3daee418cb99272ab5ce8df7b22066a6388
meryembenjelloun <benjellounmeryem2003@gmail.com>
2026-03-11 21:31:11 +0000

    video modif

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,31 +1,45 @@
-import React, {useRef} from 'react'
+import React, {useRef, useEffect} from 'react'
 import './VideoPlayer.css'
 import video from '../../assets/presentation.mp4'
+
 const VideoPlayer = ({state,setState}) => {
-    const player = useRef(null);
-    const videoRef = useRef(null);
-    
-    const close =(e) =>{
-        if(e.target === player.current){
-            if (videoRef.current) {
-                videoRef.current.pause();     
-                videoRef.current.currentTime = 0;  
-              }
-            setState(false);
-        }
+
+const player = useRef(null)
+const videoRef = useRef(null)
+
+useEffect(()=>{
+  if(state && videoRef.current){
+    videoRef.current.play()
+  }
+},[state])
+
+const close =(e)=>{
+  if(e.target === player.current){
+
+    if(videoRef.current){
+      videoRef.current.pause()
+      videoRef.current.currentTime = 0
     }
 
-  return (
-    <div className={`videoplay ${state?'': 'hide'}`} ref={player} onClick={close}>
-        <video src={video}
-         ref={videoRef}
-         controls
-         autoPlay muted loop
-        >
-        </video>
-      
-    </div>
-  )
+    setState(false)
+  }
+}
+
+return (
+<div className={`videoplay ${state ? '' : 'hide'}`} ref={player} onClick={close}>
+
+<video
+src={video}
+ref={videoRef}
+controls
+muted
+loop
+playsInline
+/>
+
+</div>
+)
+
 }
 
 export default VideoPlayer
